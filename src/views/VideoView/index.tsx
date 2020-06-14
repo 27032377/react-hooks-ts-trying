a09a727f9beeacff5832b33190bbf8f1298be863
270324377@qq.com <270324377@qq.com>
2020-06-15 00:10:37 +0800

    router & antd-mobile

--- src/views/VideoView/index.tsx
+++ src/views/VideoView/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useParams, useRouteMatch } from 'react-router-dom'
 
 export default function VideoView() {
     const { id } = useParams()
+    const routeMatch = useRouteMatch()
+    useEffect(() => {
+        console.log(routeMatch)
+    })
     return (
         <div>
             <p>
